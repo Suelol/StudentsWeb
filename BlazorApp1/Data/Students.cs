8e64223a4518d6ee68922ecbd29d26a2e907b711
Suelol <kamalovramil432@gmail.com>
2024-06-21 16:34:53 +0300

    init

---
+++ BlazorApp1/Data/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace BlazorApp1.Data
+{
+    public class Students
+    {
+        public Students(int iD, string surname, string name, string lastName, string group, DateTime dateOfBirth, string photo = "/img/user-icon.png",  bool show = false)
+        {
+            ID = iD;
+            Surname = surname;
+            Name = name;
+            LastName = lastName;
+            Group = group;
+            Photo = photo;
+            DateOfBirth = dateOfBirth;
+            Show = show;
+        }
+
+        public int ID { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Group { get; set; }
+        public string Photo { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public bool Show { get; set; }
+
+    }
+    
+}
